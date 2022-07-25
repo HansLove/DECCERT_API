b3e5a0f01ab262cc1830ec69a0ee897d34966690
Aaron Tolentino <aarontolentinotrejo@gmail>
2022-07-25 11:29:32 -0500

    .

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const ruta_1=require('./routes/route_1.js')
 const ruta_user=require('./routes/route.user.js')
 
 
-// app.use(bodyParser.urlencoded({ extended: true, limit: '10000000mb'})); 
+app.use(bodyParser.urlencoded({ extended: true, limit: '10000000mb'})); 
 app.use(bodyParser.json());
 
 app.use(express.static('./lib/views/'));
